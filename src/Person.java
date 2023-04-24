7ed3b7b896bfeaa122d27fb64a8099121779d936
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-04-24 22:19:56 +0300

    Add student

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + ' ' + lastName;
+    }
+}
