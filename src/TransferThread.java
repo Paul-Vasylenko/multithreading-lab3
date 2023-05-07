e5cd5f4a4cb3ace700f249c269e637eaf722a3b1
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-05-07 18:28:40 +0300

    add

--- src/TransferThread.java
+++ src/TransferThread.java
@@ -2,7 +2,7 @@ class TransferThread extends Thread {
     private Bank bank;
     private int fromAccount;
     private int maxAmount;
-    private static final int REPS = 1000;
+    private static final int REPS = 500000;
     public TransferThread(Bank b, int from, int max){
         bank = b;
         fromAccount = from;
@@ -10,12 +10,10 @@ class TransferThread extends Thread {
     }
     @Override
     public void run(){
-        while (true) {
-            for (int i = 0; i < REPS; i++) {
-                int toAccount = (int) (bank.size() * Math.random());
-                int amount = (int) (maxAmount * Math.random()/REPS);
-                bank.transferLocker(fromAccount, toAccount, amount);
-            }
+        for (int i = 0; i < REPS; i++) {
+            int toAccount = (int) (bank.size() * Math.random());
+            int amount = (int) (maxAmount * Math.random()/REPS);
+            bank.transferLocker(fromAccount, toAccount, amount);
         }
     }
 }
